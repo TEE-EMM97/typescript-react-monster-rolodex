7fd49a920458fb1e86fbbaa1d7f67517d2e26d8a
TEE-EMM97 <tevmc@live.com>
2022-07-15 17:00:21 +0100

    Build:

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { ChangeEvent, FC, useEffect, useState } from 'react';
+import { CardList } from './components/CardList';
+import { SearchBox } from './components/SearchBox';
+import { getData } from './utils/FetchData';
+import { Monster } from './@types/rolodex.types'
+import './index.scss'
 
-const App = () => {
+const App: FC = () => {
+  let url: string = `https://jsonplaceholder.typicode.com/users`;
+  const [searchField, setSearchField] = useState('');
+  const [monsters, setMonsters] = useState<Monster[]>([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+
+  useEffect(() => {
+      const fetchUsers = async () => {
+        const users = await getData<Monster[]>(url);
+        setMonsters(users);
+      };
+      fetchUsers();
+      return () => console.log('clean up!');
+  }, [searchField, url]);
+
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) =>
+      monster.name.toLocaleLowerCase().includes(searchField)
+    );
+
+    setFilteredMonsters(newFilteredMonsters);
+    return () => {
+      console.log('clean up!');
+    };
+  }, [monsters, searchField]);
+
+  const onSearch = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { target } = e;
+    const searchStr: string = target.value;
+    setSearchField(searchStr);
+  };
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <SearchBox onChangeHandler={onSearch} placeholder={'Search'} />
+      <CardList monsters={filteredMonsters} />
+    </>
   );
-}
+};
 
 export default App;
